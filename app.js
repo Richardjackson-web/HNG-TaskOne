0e84481fccfe90365a6efb7b8ac36754eb459410
Richardjackson-web <richardjackson556@gmail.com>
2023-09-07 16:11:00 +0100

    I made the time count in real time

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const time = document.getElementById("time");
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-const today = new Date();
-let currentDay = days[today.getDay()];
-day.innerHTML = currentDay;
+function displayTime(){
+    let refresh=1000;
+    let mytime = setTimeout('displayCurrentTime()',refresh);
+}
 
-const theTime = new Date();
-const seconds = theTime.getUTCMilliseconds();
-const minutes = theTime.getUTCMinutes();
-const hour = theTime.getUTCHours();
-const currentTime = `${hour}:${minutes}:${seconds}`;
-time.innerHTML = currentTime;
+function displayCurrentTime() {
+    const today = new Date();
+    let currentDay = days[today.getDay()];
+    day.innerHTML = currentDay;
+
+    const theTime = new Date();
+    const seconds = theTime.getUTCMilliseconds();
+    const minutes = theTime.getUTCMinutes();
+    const hour = theTime.getUTCHours();
+
+    const currentTime = `${hour}:${minutes}:${seconds}`;
+    time.innerHTML = currentTime;
+    displayTime();
+}
